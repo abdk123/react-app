3522f75bc3cc0f70b8a90b7aff765966654c5ef3
souccar <85060233+souccar-github@users.noreply.github.com>
2024-02-07 10:06:36 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Hello world</h1>
+      <p>My name is abd alrahman karaja</p>
+    </div>
+  );
+}
+
+export default App;
